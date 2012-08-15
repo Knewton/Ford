acbd0611fc651f5a4c9d18136d5c86f3288bbedb
Eric Garside <eric@knewton.com>
2012-08-15 19:39:59 -0400

    Correct version of Bsoup
    Change-Id: I43fd6a2b728eddcab61cb6385b62987153182be3

--- bin/setup/beautifulsoup.sh
+++ bin/setup/beautifulsoup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-BSOUP_URL="http://www.crummy.com/software/BeautifulSoup/bs4/download/4.0/beautifulsoup4-4.1.0.tar.gz"
+BSOUP_URL="http://pypi.python.org/packages/source/B/BeautifulSoup/BeautifulSoup-3.2.1.tar.gz"
 
 python -c "import BeautifulSoup" > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
 	echo "Installing BeautifulSoup"
 	cd /tmp/
 	rm -rf beautifulsoup*
+	rm -rf BeautifulSoup*
 	curl --silent --output beautifulsoup.tar.gz $BSOUP_URL
 	ls beautifulsoup.tar.gz
 	tar -xvzf beautifulsoup.tar.gz
 	rm beautifulsoup.tar.gz
-	cd beautifulsoup*
+	cd BeautifulSoup*
 	sudo python setup.py install
 fi
